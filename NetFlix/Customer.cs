30e91594e210ca8e79b867988db484e1a1575cd3
whhub <wanghui122004@me.com>
2016-11-21 16:37:34 +0800

    <Refactoring> [chapter01] Neflix [Version006] Inline Variable (Replace Temp with Query) 为了去掉临时变量，多了两次循环

--- NetFlix/Customer.cs
+++ NetFlix/Customer.cs
@@ -21,26 +21,40 @@ namespace NetFlix
         // 计算顾客的消费金额并打印报表
         public string Statement()
         {
-            double totalAmount = 0; // 总消费金额
-            var frequentRenterPoints = 0; // 常客积点
             var result = "Rental Record for " + Name + "\n";
 
             foreach (var rental in _rentals)
             {
-                // add frequent renter points (累计常客积点）
-                frequentRenterPoints += rental.GetFrequentRenterPoints();
-
                 // show figures for this rental (显示此笔租借记录）
                 result += "\t" + rental.Movie.Title + "\t" + rental.GetCharge() + "\n";
-
-                totalAmount += rental.GetCharge();
             }
 
             // add footer lines (结尾打印）
-            result += "Amount owed is " + totalAmount + "\n";
-            result += "You earned " + frequentRenterPoints + " frequent renter points";
+            result += "Amount owed is " + GetTotalCharge() + "\n";
+            result += "You earned " + GetFrequentRenterPoints() + " frequent renter points";
 
             return result;
+        }
+
+        private int GetFrequentRenterPoints()
+        {
+            var frequentRenterPoints = 0; // 常客积点
+            foreach (var rental in _rentals)
+            {
+                // add frequent renter points (累计常客积点）
+                frequentRenterPoints += rental.GetFrequentRenterPoints();
+            }
+            return frequentRenterPoints;
+        }
+
+        private double GetTotalCharge()
+        {
+            double totalAmount = 0; // 总消费金额
+            foreach (var rental in _rentals)
+            {
+                totalAmount += rental.GetCharge();
+            }
+            return totalAmount;
         }
     }
 }
