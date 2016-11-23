11f691a352d2ee7d147b47677d202f7618da8c5f
whhub <wanghui122004@me.com>
2016-11-23 14:23:19 +0800

    [Calculator_SimpleFacotry][version1]面向过程的写法

---
+++ Calculator_SimpleFacotry/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator_SimpleFacotry
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("请输入数字A：");
+            var A = Console.ReadLine();
+            Console.WriteLine("请输入运算符号（+、-、*、/）：");
+            var B = Console.ReadLine();
+            Console.WriteLine("请输入数字B：");
+            var C = Console.ReadLine();
+            var D = "";
+
+            if (B == "+")
+                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
+            if (B == "-")
+                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
+            if (B == "*")
+                D = Convert.ToString(Convert.ToDouble(A)*Convert.ToDouble(C));
+            if (B == "/")
+                D = Convert.ToString(Convert.ToDouble(A)/Convert.ToDouble(C));
+
+            Console.WriteLine("结果是：" + D);
+        }
+    }
+}
