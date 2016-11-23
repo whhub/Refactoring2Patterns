d8d8eaff34f9aaddcb7a908a050d651eaee728d2
whhub <wanghui122004@me.com>
2016-11-23 14:51:49 +0800

    [Calculator_SimpleFacotry][version3]Extract Method; 界面与业务 分离

--- Calculator_SimpleFacotry/Program.cs
+++ Calculator_SimpleFacotry/Program.cs
@@ -4,7 +4,7 @@ namespace Calculator_SimpleFacotry
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             Console.WriteLine("请输入数字A：");
             var strNumberA = Console.ReadLine();
@@ -12,34 +12,40 @@ namespace Calculator_SimpleFacotry
             var strOperate = Console.ReadLine();
             Console.WriteLine("请输入数字B：");
             var strNumberB = Console.ReadLine();
-            var strResult = "";
+            double result = 0;
 
             try
             {
-                switch (strOperate)
-                {
-                    case "+":
-                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) + Convert.ToDouble(strNumberB));
-                        break;
-                    case "-":
-                        strResult = Convert.ToString(Convert.ToDouble(strNumberA) - Convert.ToDouble(strNumberB));
-                        break;
-                    case "*":
-                        strResult = Convert.ToString(Convert.ToDouble(strNumberA)*Convert.ToDouble(strNumberB));
-                        break;
-                    case "/":
-                        if (strNumberB != "0")
-                            strResult = Convert.ToString(Convert.ToDouble(strNumberA)/Convert.ToDouble(strNumberB));
-                        else
-                            strResult = "除数不能为零";
-                        break;
-                }
+                var numberA = Convert.ToDouble(strNumberA);
+                var numberB = Convert.ToDouble(strNumberB);
+                result = Result(strOperate, numberA, numberB);
             }
             catch (Exception e)
             {
                 Console.WriteLine("您的输入有错：" + e.Message);
             }
-            Console.WriteLine("结果是：" + strResult);
+            Console.WriteLine("结果是：" + result);
+        }
+
+        private static double Result(string strOperate, double numberA, double numberB)
+        {
+            double result = 0;
+            switch (strOperate)
+            {
+                case "+":
+                    result = (numberA + numberB);
+                    break;
+                case "-":
+                    result = (numberA - numberB);
+                    break;
+                case "*":
+                    result = (numberA*numberB);
+                    break;
+                case "/":
+                    result = (numberA/numberB);
+                    break;
+            }
+            return result;
         }
     }
 }
