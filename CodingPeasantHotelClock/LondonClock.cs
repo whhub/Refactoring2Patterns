f0429543d9efdd760d285ccc40e11be2940c2c8f
whhub <wanghui122004@me.com>
2016-11-29 17:59:44 +0800

    [TamingTheBadCode][TDD][Version01] First Test  The_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time

---
+++ CodingPeasantHotelClock/LondonClock.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingPeasantHotelClock
+{
+    public class LondonClock
+    {
+        public int Time
+        {
+            get { return 1; }
+        }
+    }
+}
---
+++ CodingPeasantHotelClock_UT/HotelWorldClocksTest.cs
@@ -0,0 +1,21 @@
+﻿using CodingPeasantHotelClock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPeasantHotelClock_UT
+{
+    [TestClass]
+    public class HotelWorldClocksTest
+    {
+        [TestMethod]
+        public void The_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time()
+        {
+            // Arrange
+            var londonClock = new LondonClock();
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(1, londonClock.Time);
+        }
+    }
+}
