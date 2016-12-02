633b21cc6bec202b0cf9a9dfa819137d239010a8
whhub <wanghui122004@me.com>
2016-12-02 13:48:34 +0800

    [TamingTheBadCode][Trivia][Version17]  Added user intent test : The_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_even_rolling_number_but_then_correctly_for_7_times_with_odd_rolling_numbers(). Ran it and it passed.

--- Trivia_UT/GameTest.cs
+++ Trivia_UT/GameTest.cs
@@ -49,6 +49,30 @@ namespace Trivia_UT
             Assert.IsFalse(isGameStillInProgress);
         }
 
-        // TODO-user-intent: the game should be over if a player rolls the dice for 8 times and answers the question wrongly for 1 time followed by an even rolling number but then correctly for 7 times
+        [TestMethod]
+        public void
+            The_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_even_rolling_number_but_then_correctly_for_7_times_with_odd_rolling_numbers
+            ()
+        {
+            // Arrange
+            bool isGameStillInProgress = true;
+            var game = new Game();
+            game.add("Chet");
+
+            // Act
+            game.roll(1);
+            game.wrongAnswer();
+            game.roll(2);
+            game.wasCorrectlyAnswered();
+
+            for (int i = 0; i < 6; i++)
+            {
+                game.roll(1);
+                isGameStillInProgress = game.wasCorrectlyAnswered();
+            }
+
+            // Assert
+            Assert.IsFalse(isGameStillInProgress);
+        }
     }
 }
