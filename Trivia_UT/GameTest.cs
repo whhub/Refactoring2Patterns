f6031f45849aae2e77f289eadc39916649d6fa68
whhub <wanghui122004@me.com>
2016-12-02 13:32:44 +0800

    [TamingTheBadCode][Trivia][Version15]  Added user intent test : The_game_should_be_over_if_a_player_rolls_the_dice_and_answers_each_question_correctly_for_6_times(). Ran it and it passed.

--- Trivia_UT/GameTest.cs
+++ Trivia_UT/GameTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UglyTrivia;
 
 namespace Trivia_UT
 {
@@ -7,8 +8,22 @@ namespace Trivia_UT
     {
         //TODO-user-intent: the game should be over if a player rolls the dice and answers each question correctly for 6 times
         [TestMethod]
-        public void TestMethod1()
+        public void The_game_should_be_over_if_a_player_rolls_the_dice_and_answers_each_question_correctly_for_6_times()
         {
+            // Arrange
+            var game = new Game();
+            game.add("Chet");
+            bool isGameStillInProgress = true;
+
+            // Act
+            for (int i = 0; i < 6; i++)
+            {
+                game.roll(1);
+                isGameStillInProgress = game.wasCorrectlyAnswered();
+            }
+
+            // Asert
+            Assert.IsFalse(isGameStillInProgress);
         }
 
         // TODO-user-intent: the game should be over if a player rolls the dice for 7 times and answers the question wrongly for 1 time followed by an odd rolling number but then correctly for 6 times
