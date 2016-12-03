e70e66ac6e7a00f478180fd2366df414c9abec5b
whhub <wanghui122004@me.com>
2016-12-03 16:00:51 +0800

    [TamingTheBadCode][Trivia][Version85] Finish TODO: the place should be 0 if the player moves forward 12 step

--- Trivia_UT/PlayerTest.cs
+++ Trivia_UT/PlayerTest.cs
@@ -19,7 +19,6 @@ namespace Trivia_UT
             Assert.AreEqual(1, player.Place);
         }
 
-        // TODO-Working-On: the place should be 0 if the player moves forward 12 step
          [TestMethod]
         public void The_place_should_be_0_if_the_player_moves_forward_12_step()
         {
@@ -31,7 +30,24 @@ namespace Trivia_UT
 
             // Assert
             Assert.AreEqual(0, player.Place);
-        }       // TODO: the category should be Pop if the player moves 12, 4 or 8 steps
+        }       
+        // TODO-Working-on: the category should be Pop if the player moves 12, 4 or 8 steps
+        [TestMethod]
+        public void The_category_should_be_Pop_if_the_player_moves_12_4_or_8_steps()
+        {
+            // Arrange
+            var player = new Player("Chet");
+            
+            // Act, Assert
+            player.MoveForwardSteps(12);
+            Assert.AreEqual("Pop", player.CurrentCategory());
+
+            player.MoveForwardSteps(4);
+            Assert.AreEqual("Pop", player.CurrentCategory());
+
+            player.MoveForwardSteps(8);
+            Assert.AreEqual("Pop", player.CurrentCategory());
+        }
         // TODO: the category should be Pop if the player moves 1, 5 or 9 steps
         // TODO: the category should be Pop if the player moves 2, 6 or 10 steps
         // TODO: the category should be Rock if the player moves 3, 7 or 11 steps
