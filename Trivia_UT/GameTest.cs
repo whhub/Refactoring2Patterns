ba8d8f4df1ca12e859ea1bfd9e542ce38c4bbb03
whhub <wanghui122004@me.com>
2016-12-03 19:33:57 +0800

    [TamingTheBadCode][Trivia][Version96] GameTest  duplicated code to SetUp

--- Trivia_UT/GameTest.cs
+++ Trivia_UT/GameTest.cs
@@ -6,47 +6,51 @@ namespace Trivia_UT
     [TestClass]
     public class GameTest
     {
+        private Game _game;
+        private bool _isGameStillInProgress;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _game = new Game();
+            _game.add("Chet");
+            _isGameStillInProgress = true;
+        }
         [TestMethod]
         public void The_game_should_be_over_if_a_player_rolls_the_dice_and_answers_each_question_correctly_for_6_times()
         {
             // Arrange
-            var game = new Game();
-            game.add("Chet");
-            bool isGameStillInProgress = true;
 
             // Act
             for (int i = 0; i < 6; i++)
             {
-                game.roll(1);
-                isGameStillInProgress = game.wasCorrectlyAnswered();
+                _game.roll(1);
+                _isGameStillInProgress = _game.wasCorrectlyAnswered();
             }
 
             // Asert
-            Assert.IsFalse(isGameStillInProgress);
+            Assert.IsFalse(_isGameStillInProgress);
         }
 
         [TestMethod]
         public void The_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_odd_rolling_number_but_then_correctly_for_6_times()
         {
             // Arrange
-            bool isGameStillInProgress = true;
-            var game = new Game();
-            game.add("Chet");
 
             // Act
-            game.roll(1);
-            game.wrongAnswer();
-            game.roll(1);
-            game.wasCorrectlyAnswered();
+            _game.roll(1);
+            _game.wrongAnswer();
+            _game.roll(1);
+            _game.wasCorrectlyAnswered();
 
             for (int i = 0; i < 5; i++)
             {
-                game.roll(1);
-                isGameStillInProgress = game.wasCorrectlyAnswered();
+                _game.roll(1);
+                _isGameStillInProgress = _game.wasCorrectlyAnswered();
             }
 
             // Assert
-            Assert.IsFalse(isGameStillInProgress);
+            Assert.IsFalse(_isGameStillInProgress);
         }
 
         [TestMethod]
@@ -55,24 +59,21 @@ namespace Trivia_UT
             ()
         {
             // Arrange
-            bool isGameStillInProgress = true;
-            var game = new Game();
-            game.add("Chet");
 
             // Act
-            game.roll(1);
-            game.wrongAnswer();
-            game.roll(2);
-            game.wasCorrectlyAnswered();
+            _game.roll(1);
+            _game.wrongAnswer();
+            _game.roll(2);
+            _game.wasCorrectlyAnswered();
 
             for (int i = 0; i < 6; i++)
             {
-                game.roll(1);
-                isGameStillInProgress = game.wasCorrectlyAnswered();
+                _game.roll(1);
+                _isGameStillInProgress = _game.wasCorrectlyAnswered();
             }
 
             // Assert
-            Assert.IsFalse(isGameStillInProgress);
+            Assert.IsFalse(_isGameStillInProgress);
         }
     }
 }
