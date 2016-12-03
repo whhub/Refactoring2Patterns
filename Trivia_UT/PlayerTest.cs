9cada89f7b53d5a396b979614b2fb8c04853cb13
whhub <wanghui122004@me.com>
2016-12-03 16:21:16 +0800

    [TamingTheBadCode][Trivia][Version86] Failed a category test since method Player.MoveForwardSteps() is cumulative instead of restarted every time. Will change the category tests.

--- Trivia_UT/PlayerTest.cs
+++ Trivia_UT/PlayerTest.cs
@@ -31,7 +31,7 @@ namespace Trivia_UT
             // Assert
             Assert.AreEqual(0, player.Place);
         }       
-        // TODO-Working-on: the category should be Pop if the player moves 12, 4 or 8 steps
+
         [TestMethod]
         public void The_category_should_be_Pop_if_the_player_moves_12_4_or_8_steps()
         {
@@ -49,8 +49,25 @@ namespace Trivia_UT
             player.MoveForwardSteps(8);
             Assert.AreEqual(expected, player.CurrentCategory());
         }
-        // TODO: the category should be Pop if the player moves 1, 5 or 9 steps
-        // TODO: the category should be Pop if the player moves 2, 6 or 10 steps
+        // TODO-working-on: the category should be Science if the player moves 1, 5 or 9 steps
+        [TestMethod]
+        public void The_category_should_be_Science_if_the_player_moves_1_5_or_9_steps()
+        {
+            // Arrange
+            var player = new Player("Chet");
+            const string expected = "Science";
+
+            // Act, Assert
+            player.MoveForwardSteps(1);
+            Assert.AreEqual(expected, player.CurrentCategory());
+
+            player.MoveForwardSteps(5);
+            Assert.AreEqual(expected, player.CurrentCategory());
+
+            player.MoveForwardSteps(9);
+            Assert.AreEqual(expected, player.CurrentCategory());
+        }
+        // TODO: the category should be Sports if the player moves 2, 6 or 10 steps
         // TODO: the category should be Rock if the player moves 3, 7 or 11 steps
     }
 }
