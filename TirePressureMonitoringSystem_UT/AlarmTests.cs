813e12c46cb31c7c1ee1bd085e71401c9bbf269c
whhub <wanghui122004@me.com>
2016-12-04 17:50:17 +0800

    [TamingTheBadCode][TirePressureMonitoringSystem][Version004] Added 3 user intent test TODOs

--- TirePressureMonitoringSystem_UT/AlarmTests.cs
+++ TirePressureMonitoringSystem_UT/AlarmTests.cs
@@ -6,6 +6,9 @@ namespace TirePressureMonitoringSystem_UT
     public class AlarmTests
     {
         // TODO-new-feature: a normal pressure value after a value outside the range should stop the alarm
+        // TODO-user-intent-test: a normal pressure value should not raise the alarm
+        // TODO-user-intent-test: a pressure value outside the range should raise the alarm
+        // TODO-user-intent-test: a normal pressure value after a value outside the range should not stop the alarm
         [TestMethod]
         public void TestMethod1()
         {
