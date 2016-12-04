5e94522e1f2934fc9ef30c0f729801bc26937c29
whhub <wanghui122004@me.com>
2016-12-04 22:17:25 +0800

    [TamingTheBadCode][TirePressureMonitoringSystem][Version005] working-on: a normal pressure value should not raise the alarm

--- TirePressureMonitoringSystem_UT/AlarmTests.cs
+++ TirePressureMonitoringSystem_UT/AlarmTests.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TirePressureMonitoringSystem;
 
 namespace TirePressureMonitoringSystem_UT
 {
     [TestClass]
     public class AlarmTests
     {
+        private Alarm _alarm = new Alarm();
         // TODO-new-feature: a normal pressure value after a value outside the range should stop the alarm
-        // TODO-user-intent-test: a normal pressure value should not raise the alarm
+        // TODO-user-intent-test-working-on: a normal pressure value should not raise the alarm
         // TODO-user-intent-test: a pressure value outside the range should raise the alarm
         // TODO-user-intent-test: a normal pressure value after a value outside the range should not stop the alarm
         [TestMethod]
-        public void TestMethod1()
+        public void A_normal_pressure_value_should_not_raise_the_alarm()
         {
+            // Arrange
+            
+
+            // Assert
+            Assert.IsFalse(_alarm.AlarmOn);
         }
     }
 }
