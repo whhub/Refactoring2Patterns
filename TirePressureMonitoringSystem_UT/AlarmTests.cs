3ebbbab3e51095453b1fa5b9b3eba86f2f3ad6e2
whhub <wanghui122004@me.com>
2016-12-04 17:33:54 +0800

    [TamingTheBadCode][TirePressureMonitoringSystem][Version001] Added TODO-new-feature: the alarm will be turned off before each checking of pressure

---
+++ TirePressureMonitoringSystem_UT/AlarmTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TirePressureMonitoringSystem_UT
+{
+    [TestClass]
+    public class AlarmTests
+    {
+        // TODO-new-feature: the alarm will be turned off before each checking of pressure
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
