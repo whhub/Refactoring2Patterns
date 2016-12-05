7b37d78fdc175f410ae3fa4ccc870217a42f2d88
whhub <wanghui122004@me.com>
2016-12-05 10:34:37 +0800

    [TamingTheBadCode][TirePressureMonitoringSystem][Version018] Fixed compiler error: Made class Sensor implement interface Transducer

--- TirePressureMonitoringSystem/Sensor.cs
+++ TirePressureMonitoringSystem/Sensor.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace TirePressureMonitoringSystem
 {
-    public class Sensor
+    public class Sensor : Transducer
     {
         //
         // The reading of the pressure value from the sensor is simulated in this implementation.
