e18bd1ad97a711db4eacea974e868965fcf58811
whhub <wanghui122004@me.com>
2016-12-05 11:55:06 +0800

    [TamingTheBadCode][Mock][TurnTicket][Version01] Added TODO: Depending on a static method violates the Dependency Inversion Priciple and Open-Closed Principle.

--- TicketDispenser/TicketDispenser.cs
+++ TicketDispenser/TicketDispenser.cs
@@ -5,6 +5,7 @@ namespace TicketDispenser
 
         public TurnTicket GetTurnTicket()
         {
+            // TODO: Depending on a static method violates the Dependency Inversion Priciple and Open-Closed Principle.
             int newTurnNumber = TurnNumberSequence.GetNextTurnNumber();
             var newTurnTicket = new TurnTicket(newTurnNumber);
 
