125c6add25098d113097c61d5aa701674f208ed3
whhub <wanghui122004@me.com>
2016-12-05 13:55:54 +0800

    [TamingTheBadCode][Mock][TurnTicket][Version04] Added an unit test TODO for class TicketDispenser

--- TicketDispenser_UT/TicketDispenserTests.cs
+++ TicketDispenser_UT/TicketDispenserTests.cs
@@ -12,5 +12,8 @@ namespace TicketDispenser_UT
         // TODO-user-intent-test: a new ticket should have the turn number subsequent to the previous ticket
 
         // TODO-user-intent-test: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
+
+        // TODO-unit-test: the ticket dispenser shouble dispense the ticket number 11 if give a turn number 11 to it
+
     }
 }
