548b2ac1b44b964c3c9b5cfcc7719f56ad3292f7
whhub <wanghui122004@me.com>
2016-12-05 19:38:25 +0800

    [TamingTheBadCode][Chapter18 Real UT][HtmlTextConverter][Version04] Added 3 user intent test TODOs

--- Textconverter_UT/UnicodeFileToHtmTextConverterTests.cs
+++ Textconverter_UT/UnicodeFileToHtmTextConverterTests.cs
@@ -7,5 +7,11 @@ namespace Textconverter_UT
     {
         // TODO-new-feature: Make the UnicodeFileToHtmTextConverter working for not only a file but also a string
 
+        // TODO-user-intent-test: should convert ampersand
+
+        // TODO-user-intent-test: should convert greater than and less than
+        
+        // TODO-user-intent-test: should add a line break for a new line
+
     }
 }
