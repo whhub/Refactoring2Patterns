57e73df2e3815c81257e9e56c03eb32ffdfc9174
whhub <wanghui122004@me.com>
2016-12-05 09:50:24 +0800

    [TamingTheBadCode][TirePressureMonitoringSystem][Version007] replace field with local variable

--- TirePressureMonitoringSystem_UT/AlarmTests.cs
+++ TirePressureMonitoringSystem_UT/AlarmTests.cs
@@ -6,7 +6,6 @@ namespace TirePressureMonitoringSystem_UT
     [TestClass]
     public class AlarmTests
     {
-        private Alarm _alarm = new Alarm();
         // TODO-new-feature: a normal pressure value after a value outside the range should stop the alarm
         // TODO-user-intent-test-working-on: a normal pressure value should not raise the alarm
         // TODO-user-intent-test: a pressure value outside the range should raise the alarm
@@ -15,10 +14,13 @@ namespace TirePressureMonitoringSystem_UT
         public void A_normal_pressure_value_should_not_raise_the_alarm()
         {
             // Arrange
-            _alarm.Check(); 
+            Alarm alarm = new Alarm();
+            
+            // Act
+            alarm.Check(); 
 
             // Assert
-            Assert.IsFalse(_alarm.AlarmOn);
+            Assert.IsFalse(alarm.AlarmOn);
         }
     }
 }
