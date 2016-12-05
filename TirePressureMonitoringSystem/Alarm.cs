7cc9a439b71231158de3c491f88436766db7f04c
whhub <wanghui122004@me.com>
2016-12-05 10:32:59 +0800

    [TamingTheBadCode][TirePressureMonitoringSystem][Version017] Wrote intention code for   TODO: Depending on a concrete Sensor violates the Dependency Inversion Principle and Open-Closed Principle

--- TirePressureMonitoringSystem/Alarm.cs
+++ TirePressureMonitoringSystem/Alarm.cs
@@ -4,24 +4,24 @@ namespace TirePressureMonitoringSystem
     {
         public const double LowPressureThreshold = 17;
         private const double HighPressureThreshold = 21;
-        // TODO: Depending on a concrete Sensor violates the Dependency Inversion Principle and Open-Closed Principle
-        private readonly Sensor _sensor ;
+        // TODO-working-on: Depending on a concrete Sensor violates the Dependency Inversion Principle and Open-Closed Principle
+        private Transducer _transducer;
 
         public Alarm()
         {
-            _sensor = new Sensor();
+            _transducer = new Sensor();
         }
 
         public Alarm(Transducer transducer)
         {
-
+            _transducer = transducer;
         }
 
         public bool AlarmOn { get; private set; }
 
         public void Check()
         {
-            var psiPressureValue = _sensor.PopNextPressurePsiValue();
+            var psiPressureValue = _transducer.PopNextPressurePsiValue();
 
             if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
             {
