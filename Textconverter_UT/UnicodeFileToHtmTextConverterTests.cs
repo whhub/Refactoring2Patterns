b52861a66dac7fc0c25221f9b154e9e881619273
whhub <wanghui122004@me.com>
2016-12-05 19:43:15 +0800

    [TamingTheBadCode][Chapter18 Real UT][HtmlTextConverter][Version05] Working on         // TODO-user-intent-test: should convert ampersand    Wrote test Should-covert-ampersand() for it with the Assert part.

--- Textconverter_UT/UnicodeFileToHtmTextConverterTests.cs
+++ Textconverter_UT/UnicodeFileToHtmTextConverterTests.cs
@@ -7,7 +7,13 @@ namespace Textconverter_UT
     {
         // TODO-new-feature: Make the UnicodeFileToHtmTextConverter working for not only a file but also a string
 
-        // TODO-user-intent-test: should convert ampersand
+        // TODO-user-intent-test-working-on: should convert ampersand
+        public void Should_covert_ampersand()
+        {
+            
+            // Assert
+            Assert.AreEqual("", convert.ConvertToHtml());
+        }
 
         // TODO-user-intent-test: should convert greater than and less than
         
