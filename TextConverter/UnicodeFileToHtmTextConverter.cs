a126281abdf4ed66806dd2230d4f5d235b08eeb7
whhub <wanghui122004@me.com>
2016-12-05 22:19:24 +0800

    [TamingTheBadCode][Chapter18 Real UT][HtmlTextConverter][Version10] Fixing null pointer exception: Wrote intention code in method HtmlTextConverter.ConvertToHtml() to depend on the interface Reader instead of a concrete FileReader Object

--- TextConverter/UnicodeFileToHtmTextConverter.cs
+++ TextConverter/UnicodeFileToHtmTextConverter.cs
@@ -6,6 +6,7 @@ namespace TextConverter
     public class UnicodeFileToHtmTextConverter
     {
         private readonly string _fullFilenameWithPath;
+        private TextReader _reader;
 
         public UnicodeFileToHtmTextConverter(string fullFilenameWithPath)
         {
@@ -14,23 +15,20 @@ namespace TextConverter
 
         public UnicodeFileToHtmTextConverter(TextReader reader)
         {
-
+            _reader = reader;
         }
 
         public string ConvertToHtml()
         {
-            // TODO: Depending on the file system violates the Dependency Inversion Principle and Open-closed Principle.
-            var reader = new StreamReader(new FileStream(_fullFilenameWithPath, FileMode.Open));
-
             var html = string.Empty;
 
-            var line = reader.ReadLine();
+            var line = _reader.ReadLine();
             while (line != null)
             {
                 // TODO: Depending on the third party library violates the Dependency Inversion Principle and Open-Closed Principle.
                 html += HttpUtility.HtmlEncode(line);
                 html += "<br />";
-                line = reader.ReadLine();
+                line = _reader.ReadLine();
             }
 
             return html;
