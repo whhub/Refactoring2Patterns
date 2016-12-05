cb1e7a70c61ce70542afd27cc51caac641b62a60
whhub <wanghui122004@me.com>
2016-12-05 12:00:17 +0800

    [TamingTheBadCode][Mock][TurnTicket][Version02] Added two new feature TODOs for the different sequence starting numbers of VIP and regular customers.

---
+++ TicketDispenser_UT/TicketDispenserTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketDispenser_UT
+{
+    [TestClass]
+    public class TicketDispenserTests
+    {
+        // TODO-new-feature: the turn number sequence of the vip customers starts from 1001
+
+        // TODO-new-feature: the turn number sequence of the regular customers starts from 2001
+    }
+}
