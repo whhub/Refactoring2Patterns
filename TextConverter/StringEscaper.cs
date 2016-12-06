d0d7378a6c6436b3bb0d493efeb7e497ea76f962
whhub <wanghui122004@me.com>
2016-12-06 10:26:53 +0800

    [TamingTheBadCode][Chapter18 Real UT][HtmlTextConverter][Version14] Fixed Compiler error: Created constructor UnicodeFileToHtmlTextConverter(Reader, StringEscaper)

--- TextConverter/StringEscaper.cs
+++ TextConverter/StringEscaper.cs
@@ -1,7 +1,12 @@
-﻿namespace TextConverter
+﻿using System.Web;
+
+namespace TextConverter
 {
     public class StringEscaper
     {
-
+        public string EscapeHtml(string line)
+        {
+            return HttpUtility.HtmlEncode(line);
+        }
     }
 }
--- TextConverter/UnicodeFileToHtmTextConverter.cs
+++ TextConverter/UnicodeFileToHtmTextConverter.cs
@@ -6,15 +6,20 @@ namespace TextConverter
     public class UnicodeFileToHtmTextConverter
     {
         private TextReader _reader;
+        private StringEscaper _stringEscaper;
 
-        public UnicodeFileToHtmTextConverter(string fullFilenameWithPath)
+        public UnicodeFileToHtmTextConverter(string fullFilenameWithPath) : this(new StreamReader(new FileStream(fullFilenameWithPath, FileMode.Open)))
         {
-            _reader = new StreamReader(new FileStream(fullFilenameWithPath, FileMode.Open));
         }
 
-        public UnicodeFileToHtmTextConverter(TextReader reader)
+        public UnicodeFileToHtmTextConverter(TextReader reader) : this(reader, new StringEscaper())
+        {
+        }
+
+        public UnicodeFileToHtmTextConverter(TextReader reader, StringEscaper stringEscaper)
         {
             _reader = reader;
+            _stringEscaper = stringEscaper;
         }
 
         public string ConvertToHtml()
@@ -25,7 +30,7 @@ namespace TextConverter
             while (line != null)
             {
                 // TODO-working-on: Depending on the third party library violates the Dependency Inversion Principle and Open-Closed Principle
-                html += _stringEscapter.EscapeHtml(line);
+                html += _stringEscaper.EscapeHtml(line);
                 html += "<br />";
                 line = _reader.ReadLine();
             }
