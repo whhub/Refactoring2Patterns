b69c6fe927204e0505696de632c0719582d6527f
whhub <wanghui122004@me.com>
2017-01-05 07:57:42 +0800

    [DesignPattens][Creation][Maze][Version005][feat] Door

---
+++ Maze/Door.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Maze
+{
+    internal class Door : MapSite
+    {
+        private bool _isOpen;
+        private Room _room1;
+        private Room _room2;
+
+        public Door(Room room1, Room room2)
+        {
+            _room1 = room1;
+            _room2 = room2;
+        }
+
+        #region Overrides of MapSite
+
+        public override void Enter()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public Room OtherSideFrom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
