a60f0087b95d7570e71e826456dd649ee25502d0
whhub <wanghui122004@me.com>
2017-01-05 07:49:04 +0800

    [DesignPattens][Creation][Maze][Version003][feat] Room as a Mapsite has 4 neighbor Mapsite

---
+++ Maze/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Maze
+{
+    internal class Room : MapSite
+    {
+        private int _roomNumber;
+        private readonly MapSite[] _sides = new MapSite[4];
+
+        public Room(int roomNumber)
+        {
+            _roomNumber = roomNumber;
+        }
+
+        public MapSite this[Directions direction]
+        {
+            get { return _sides[(int) direction]; }
+            set { _sides[(int) direction] = value; }
+        }
+
+        #region Overrides of MapSite
+
+        public override void Enter()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
