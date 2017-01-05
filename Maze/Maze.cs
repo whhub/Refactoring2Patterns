f8f4755012c268257be079f683245ece3c8fcdfd
whhub <wanghui122004@me.com>
2017-01-05 08:02:10 +0800

    [DesignPattens][Creation][Maze][Version006][feat] Maze as a collection of Room

---
+++ Maze/Maze.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Maze
+{
+    internal class Maze
+    {
+        public void AddRoom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Room RoomNO(int NO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
